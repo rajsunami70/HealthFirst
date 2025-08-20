680794b44f949ebc7ff9b0f29523edd171b0117a
rajsunami70 <rajsunami369@gmail.com>
2025-08-21 00:16:13 +0530

    Project

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ const notificationRouter = require("./routes/notificationRouter");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://healthfirst-ui3i.onrender.com"],
+  credentials: true
+}));
+
 
 
 app.use(express.json());
